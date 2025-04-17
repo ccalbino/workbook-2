5001ee02853839904080b28589375719c33360c8
ccalbino <christian.albino89@gmail.com>
2025-04-17 13:42:34 -0400

    Full name app

---
+++ FullNameGenerator/src/main/java/com/pluralsight/FullNameApplication.java
@@ -0,0 +1,71 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FullNameApplication {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter you name");
+
+        System.out.print("First name: ");
+        String firstName = scanner.nextLine().trim();
+
+
+        System.out.print("Middle name: ");
+        String middleName = scanner.nextLine().trim();
+
+
+
+        System.out.print("Last name: ");
+        String lastName = scanner.nextLine().trim();
+
+
+
+        System.out.print("Suffix: ");
+        String suffix = scanner.nextLine().trim();
+
+
+        //compute the unknown
+
+        String fullName = "";
+
+        if(middleName.isBlank()){
+            fullName = firstName +  " " + lastName; //+ suffix;
+
+        }
+        else{
+            fullName = firstName + " " + middleName + " " + lastName;
+
+        }
+        if (!suffix.isBlank()){
+            fullName += ", " + suffix;
+        }
+
+        System.out.println("Full name: " + fullName);
+//
+//        if (middleName.isEmpty()) {
+//            fullName = fullName +  " " + lastName + suffix;
+//        }
+//
+//        if (suffix.isEmpty()) {
+//            fullName = fullName + " " + lastName;
+//
+//        }
+//
+//        System.out.println("Full name: " + fullName);
+
+
+
+
+    }
+            
+            
+        }
+
+
+
+
+
+
