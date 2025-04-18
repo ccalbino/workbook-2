3e43bcf4a4ad4528626ee40f3218d9c8994a4d1e
ccalbino <christian.albino89@gmail.com>
2025-04-18 13:04:32 -0400

    consume the class

--- ClassDemo1/src/main/java/com/pluralsight/Main.java
+++ ClassDemo1/src/main/java/com/pluralsight/Main.java
@@ -6,28 +6,58 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String fullName;
+        int age;
+        String profession;
+
+
+
         System.out.print("What is the person's full name: ");
-        String fullName = scanner.nextLine();
+        fullName = scanner.nextLine();
 
         System.out.print("What is the person's age: ");
-        int age = scanner.nextInt();
+        age = scanner.nextInt();
         scanner.nextLine(); //clear CRLF meaning the line after can run a string due to this one being a int
 
         System.out.print("What is the person's profession: ");
-        String profession = scanner.nextLine();
+        profession = scanner.nextLine();
+
+        Person thePerson = new Person(fullName, age, profession);
 
 
-        //these are both methods: We need to define it below to see it
-        displayPerson(fullName, age, profession);
-        savePerson(fullName, age, profession);
+
+
+        System.out.print("What is the person's full name: ");
+         fullName = scanner.nextLine();
+
+        System.out.print("What is the person's age: ");
+         age = scanner.nextInt();
+        scanner.nextLine(); //clear CRLF meaning the line after can run a string due to this one being a int
+
+        System.out.print("What is the person's profession: ");
+         profession = scanner.nextLine();
+
+
+
+        Person theOtherPerson = new Person(fullName, age, profession);
+
+        System.out.println(thePerson.getFullName());
+
+        System.out.println(theOtherPerson.getFullName());
+
+
+
+//        //these are both methods: We need to define it below to see it
+        displayPerson(thePerson);
+        displayPerson(theOtherPerson);
 
 
 
     }
 
     /// define display person method. public static void because it is a method
-    public static void displayPerson(String fullName, int age, String profession){
-        System.out.printf("Person %s is a %s and is %d years old", fullName, profession, age);
+    public static void displayPerson(Person person){
+        System.out.printf("Person %s is a %s and is %d years old", person.getFullName(), person.getProfession(), person.getAge());
 
     }
 
